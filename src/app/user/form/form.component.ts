b38b9ae79656131fbfe45ef2dc9760bfce8a0b5d
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2022-10-11 16:19:02 +0100

    login/signup form logic

--- src/app/user/form/form.component.ts
+++ src/app/user/form/form.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  FormGroup,
+  FormControl,
+  Validator,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -8,7 +15,70 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class FormComponent implements OnInit {
 
-  ngOnInit(): void {
+
+
+  constructor(private auth: AngularFireAuth,
+    private fb: FormBuilder) {
+
   }
+
+  form: FormGroup | undefined;
+  status = "login" || "signin" || "reset";
+  isLoading = false;
+
+  serverResponde = '';
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [Validators.required, Validators.minLength(6)]
+      ],
+      passwordConfirmation: ['', []]
+    })
+  }
+
+
+  get isUserSignup() {
+    return this.status === "signup";
+  }
+
+  get isPassResset() {
+    return this.status === "reset"
+  }
+
+  get isUserLoggedIn() {
+    return this.status === "login"
+  }
+
+  get email() {
+    return this.form?.get('email')
+  }
+
+  get password() {
+    return this.form?.get('password')
+  }
+
+  get passwordConfirmation() {
+    return this.form?.get('passwordConfirmation')
+  }
+
+
+  get isPasswordMatch() {
+    if (this.status !== 'signup') {
+      return true;
+    } else {
+      return this.password?.value === this.passwordConfirmation?.value;
+    }
+  }
+
+
+  onUserSubmit() {
+
+  }
+
+
+
 
 }
