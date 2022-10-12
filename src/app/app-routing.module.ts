f6c9423c2b430ee68a8324c1e1ec04074cad35ac
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2022-10-12 12:01:27 +0100

    add kanban module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,8 @@ const routes: Routes = [
     path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)
   },
   {
-    path: 'signup', loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+    path: "kanban", loadChildren: () => import('./kanban/kanban.module').then(m => m.KanbanModule)
+    , canActivate: [LoggedInGuard]
   }
 ];
 
---
+++ src/app/kanban/kanban.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { KanbanRoutingModule } from './kanban-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatButtonToggle } from '@angular/material/button-toggle'
+import { MatDialogModule } from '@angular/material/dialog';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    KanbanRoutingModule,
+    SharedModule,
+    FormsModule,
+    DragDropModule,
+    MatDialogModule,
+    MatButtonToggle
+
+  ]
+})
+export class KanbanModule { }
